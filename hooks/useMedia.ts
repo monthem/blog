f75412e80686a70c2306268d077d1f8194c4f3bc
monthem <lpoeh01@gmail.com>
2020-12-18 18:24:33 +0900

    OPTIMIZE: optimized useMedia hook by adding optional delay and by comparing prevValue is different from curValue, ie, when curValue and prevValue is same, it doesn't call hook update

--- hooks/useMedia.ts
+++ hooks/useMedia.ts
@@ -1,24 +1,43 @@
 import React from "react";
-import getMatchingScreen from "../utils/layout";
+import getMatchingScreen, { ScreenSize } from "../utils/layout";
 
 type UseMediaOption<T> = {
   [screen in ReturnType<typeof getMatchingScreen>]?: T;
 } & {
-  defaultValue?: T;
+  config?: {
+    defaultValue?: T;
+    delay?: number;
+  }
 }
 
-const useMedia = <T>(option: UseMediaOption<T>) => {
-  const availableValue = option[Object.values(option).findIndex((value) => value !== undefined)];
-  const defaultValue = option.defaultValue || availableValue;
-  const [value, set] = React.useState<T>(defaultValue);
+const useMedia = <T>(option: UseMediaOption<T>): T | undefined => {
+  const {config = {}} = option;
+  const {delay = 0} = config;
+  const keys = Object.keys(option) as ScreenSize[];
+  const availableValueIndex = Object.values(option).findIndex((value) => value !== undefined);
+  const availableValue = option[keys[availableValueIndex]];
+  const defaultValue = option.config?.defaultValue || availableValue;
+  const [value, set] = React.useState<T | undefined>(defaultValue);
+  const prevValue = React.useRef(value);
+  const update = React.useRef<number | null>(null);
   React.useEffect(() => {
-    const handler = (e: UIEvent) => {
+    const handler = () => {
       const matchingScreen = getMatchingScreen(window.innerWidth);
       const matchingValue = option[matchingScreen] || defaultValue;
-      set(matchingValue);
+      if (prevValue.current !== matchingValue) {
+        if (update.current !== null) clearTimeout(update.current);
+        update.current = setTimeout(() => {
+          prevValue.current = matchingValue;
+          set(matchingValue);
+        }, delay)
+      }
     }
+    handler();
     window.addEventListener("resize", handler);
-    return () => window.removeEventListener("resize", handler);
+    return () => {
+      window.removeEventListener("resize", handler);
+      if (update.current) clearTimeout(update.current);
+    }
   }, [])
   return value || defaultValue;
 }
