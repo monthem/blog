0d167a5e95bd6b3f45563646b2690570224bb915
monthem <lpoeh01@gmail.com>
2020-12-18 18:22:51 +0900

    OPTIMIZE: optimized useMeasure hook by adding optional delay which waits until size change stops. (MISC: added onMeasureStart, onMeasuerEnd prop to useMeasure hook option)

--- hooks/useMeasure.ts
+++ hooks/useMeasure.ts
@@ -1,13 +1,35 @@
 import React from "react";
 import ResizeObserver from 'resize-observer-polyfill';
 
-type ObserverRect = Pick<DOMRect, "left" | "top" | "width" | "height">
+export type ObserverRect = Pick<DOMRect, "left" | "top" | "width" | "height">;
+type MeasureResult<T extends Element> = [{ref: React.RefObject<T>}, ObserverRect];
+type MeasureOption = {
+  /**@param delay this is used for preventing too many updates.*/
+  delay?: number;
+  onMeasureEnd?: (rect: ObserverRect) => any;
+  onMeasureStart?: () => any;
+}
 
-const useMeasure = <T extends HTMLElement>() => {
-  const ref = React.useRef<T>();
+const useMeasure = <T extends HTMLElement>(option?: MeasureOption): MeasureResult<T> => {
+  const {delay = 0, onMeasureEnd, onMeasureStart} = option || {};
+  const ref = React.useRef<T>(null);
   const [bounds, set] = React.useState<ObserverRect>({left: 0, height: 0, top: 0, width: 0});
-  const [ro] = React.useState(() => new ResizeObserver(([entry]) => set(entry.contentRect)));
-  React.useEffect(() => (ro.observe(ref.current), ro.disconnect), []);
+  const latestUpdate = React.useRef<number | null>(null);
+  const ro = React.useRef(new ResizeObserver(([entry]) => {
+    if (onMeasureStart) onMeasureStart();
+    if (latestUpdate.current) clearTimeout(latestUpdate.current);
+    latestUpdate.current = setTimeout(() => {
+      set(entry.contentRect);
+      if (onMeasureEnd) onMeasureEnd(entry.contentRect);
+    }, delay)
+  })).current;
+  React.useEffect(() => {
+    if (ref.current) ro.observe(ref.current);
+    return () => {
+      ro.disconnect();
+      if (latestUpdate.current) clearTimeout(latestUpdate.current);
+    };
+  }, []);
   return [{ref}, bounds];
 }
 
